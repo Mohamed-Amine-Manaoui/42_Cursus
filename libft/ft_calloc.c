e75f1c6bcbcf4a9cb29137f6b75dd7d63f6448d9
Mohamed Amine Manaoui <mohamedamine.manaoui@gmail.com>
2023-11-04 01:13:16 +0100

    libft

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -4,4 +4,12 @@ void *ft_calloc(size_t num, size_t size)
 {
     if (num == 0 || size == 0)
     return NULL;
+    int *ptr;
+
+    ptr = malloc(size * num);
+    if (ptr != 0)
+    {
+        ft_bzero(ptr,size * num);
+    }
+    return ptr
 }
