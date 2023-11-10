57d12b60467e290bd7c669bb755aa3dd54bbcbfd
Mohamed Amine Manaoui <mmanaoui@e2r8p6.1337.ma>
2023-11-10 18:44:18 +0100

    libft

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -5,12 +5,60 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmanaoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/10 17:24:32 by mmanaoui          #+#    #+#             */
-/*   Updated: 2023/11/10 17:28:25 by mmanaoui         ###   ########.fr       */
+/*   Created: 2023/11/10 18:42:10 by mmanaoui          #+#    #+#             */
+/*   Updated: 2023/11/10 18:42:37 by mmanaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmanaoui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 17:24:32 by mmanaoui          #+#    #+#             */
+/*   Updated: 2023/11/10 18:41:24 by mmanaoui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char ft_ftest(unsigned int a, char b)
+{
+	if (b >= 'a' && b <= 'z')
+		return (b - 32);
+	return (b);
+}
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	int i;
+	char *ptr;
+	if (!s || !f)
+		return (NULL);
+	ptr = (char *)malloc(ft_strlen(s));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		ptr[i] = f(i,s[i]);
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+int main() {
+    const char *input_string = "Hello, World!";
 
+    char *result = ft_strmapi(input_string, ft_ftest);
+
+
+    printf("Chaîne d'origine : %s\n", input_string);
+    printf("Nouvelle chaîne   : %s\n", result);
+
+    // Libération de la mémoire allouée
+    free(result);
+
+    return 0;
 }
