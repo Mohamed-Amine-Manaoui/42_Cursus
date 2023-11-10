2cf7dfb92ac6720be4268279e65b3d28cf79cad8
Mohamed Amine Manaoui <mmanaoui@e2r8p6.1337.ma>
2023-11-10 21:33:50 +0100

    ft_itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mmanaoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 11:28:47 by mmanaoui          #+#    #+#             */
-/*   Updated: 2023/11/05 11:40:47 by mmanaoui         ###   ########.fr       */
+/*   Updated: 2023/11/10 21:33:02 by mmanaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,61 +14,74 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-int ft_count(int n)
-{
-    int temp;
-    int count;
-    char *ptr;
 
-    count = 1;
-    if (n < 0)
-    {
-        n = -n;
-        count++; 
-    }
-    while (n > 9)
-    {
-        n /= 10;
-        count++;
-    }
-    return count;
+#include "libft.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_count(int n)
+{
+	int		temp;
+	int		count;
+	char	*ptr;
+
+	count = 1;
+	if (n < 0)
+	{
+		n = -n;
+		count++;
+	}
+	while (n > 9)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
 }
 
-char *ft_itoa(int n)
+void	ft_while(char *ptr, int n, int i)
 {
-    int temp;
-    int count;
-    char *ptr;
-    int i;
-    int is_negative;
-
-    count = ft_count(n);
-    ptr = (char *)malloc(count +1);
-    is_negative = 0;
-
-    if (n < 0)
-    {
-        n = -n;
-        is_negative = 1;
-    }
-    ptr[count] = '\0';
-    i = count - 1;
-    while (i >=0)
-    {
-        ptr[i] =((n % 10) + '0');
-        n/= 10;
-        i--;
-    }
-    if(is_negative)
-        ptr[0] = '-';
-    return (ptr);
+	while (i >= 0)
+	{
+		ptr[i] = ((n % 10) + '0');
+		n /= 10;
+		i--;
+	}
 }
-/*
-int main() {
-    int num1 = 0;
 
-    char *str1 = ft_itoa(num1);
-        printf("Number: %d, String: %s\n", num1, str1);
-        free(str1);
-    }
-*/
+char	*ft_allocate(int n)
+{
+	char	*ptr;
+
+	ptr = (char *)malloc(ft_count(n));
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
+
+char	*ft_itoa(int n)
+{
+	int		temp;
+	int		count;
+	int		i;
+	int		is_negative;
+	char	*ptr;
+
+	count = ft_count(n);
+	ptr = ft_allocate(n);
+	is_negative = 0;
+	if (n < 0)
+	{
+		n = -n;
+		is_negative = 1;
+	}
+	if (n == -2147483648)
+		return (ft_strdup((const char *)n));
+	ptr[count] = '\0';
+	i = count - 1;
+	ft_while(ptr, n, i);
+	if (is_negative)
+		ptr[0] = '-';
+	return (ptr);
+}
