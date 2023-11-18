c145f76ce67442b41243d040d469fbb8b9211b1b
Mohamed <mohamedamine.manaoui@gmail.com>
2023-11-18 15:50:58 +0100

    libft

--- libft/ft_lstdelone_bonus.c
+++ libft/ft_lstdelone_bonus.c
@@ -1,39 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmanaoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/01 11:17:37 by mmanaoui          #+#    #+#             */
-/*   Updated: 2023/11/18 15:30:47 by mmanaoui         ###   ########.fr       */
+/*   Created: 2023/11/17 15:53:36 by mmanaoui          #+#    #+#             */
+/*   Updated: 2023/11/17 16:28:46 by mmanaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	size_t	i;
-
-	i = 0;
-	if ((char *)dest == (char *)src)
-		return ((char *)src);
-	if (!dest && !src)
-		return (NULL);
-	while (i < n)
+	if (lst && del)
 	{
-		((char *)dest)[i] = ((const char *)src)[i];
-		i++;
+		del (lst->content);
+		free(lst);
 	}
-	return (dest);
 }
-/*
-int main ()
-{
-    char src[] = "amine";
-    char dest[10];
-    ft_memcpy(dest, src, 3);
-    printf("%s",dest);
-}
-*/
---
+++ libft/ft_split.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmanaoui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/18 14:46:33 by mmanaoui          #+#    #+#             */
+/*   Updated: 2023/11/18 15:34:05 by mmanaoui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int    ft_count(const char *s, char c)
+{
+    size_t    count;
+    size_t    i;
+
+    count = 0;
+    i = 0;
+    while (s[i])
+    {
+        if (s[i] != c)
+        {
+            count++;
+            while (s[i] && s[i] != c)
+                i++;
+        }
+        else if (s[i] == c)
+            i++;
+	}
+    return (count);
+}
+
+static char    **ft_checknull(char **str, int n)
+{
+    while (n > 0)
+    {
+        free(str[--n]);
+    }
+    free(str);
+    return (0);
+}
+
+char    **ft_split(char const *s, char c)
+{
+    int        i;
+    int        n;
+    int        j;
+    char    **str;
+
+    str = (char **)malloc((ft_count(s, c) + 1) * sizeof(char *));
+    if (str == NULL)
+        return (NULL);
+    i = 0;
+    n = 0;
+    while (s[i] != '\0')
+    {
+        while (s[i] == c)
+            i++;
+        if (s[i] == '\0')
+            break ;
+        j = 0;
+        while (s[i] != c && s[i] != '\0' && j++ > -1)
+            i++;
+        str[n++] = ft_substr(s, i - j, j);
+        if (!str[n - 1])
+            return (ft_checknull(str, n));
+    }
+    str[n] = NULL;
+    return (str);
+}
+/*int main()
+{
+    char *test = "hello world";
+    char ptr = ' ';
+    char **test2 = ft_split(test, ptr);
+    int i = 0;
+    while (test2[i] != NULL)
+    {
+        printf("%s\n", test2[i]);
+        i++;
+    }
+
+    return 0;
+}*/
