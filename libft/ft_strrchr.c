cc38001933b403a7df4918ff636665ae6211417f
Mohamed <mohamedamine.manaoui@gmail.com>
2023-12-10 02:52:56 +0100

    printf

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: mmanaoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 17:45:17 by mmanaoui          #+#    #+#             */
-/*   Updated: 2023/11/23 20:37:08 by mmanaoui         ###   ########.fr       */
+/*   Updated: 2023/12/10 02:48:09 by mmanaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ char	*ft_strrchr(const char *str, int c)
 	i = ft_strlen(str);
 	while (i >= 0)
 	{
-		if (str[i] == c)
+		if (str[i] == (char)c)
 			return ((char *)(str + i));
 		i--;
 	}
-	if (str[i] == c)
+	if (str[i] == (char)c)
 		return ((char *)(str + i));
 	return (0);
 }
